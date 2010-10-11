5adbebaa8f823a55b4b3a73c6e9289b2babe3f06
Ryan Hartford <hartfordr@gmail.com>
2010-10-11 16:54:48 -0700

    added push backend send functionality

--- lib/rapidsms/backends/push.py
+++ lib/rapidsms/backends/push.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse, HttpResponseBadRequest
 import datetime
 from urllib import urlencode
 from urllib2 import urlopen
+from elementtree import ElementTree as ET
+import urllib2
 
 class Push(RapidHttpBacked):
     """ A RapidSMS backend for PUSH SMS
@@ -23,7 +25,6 @@ class Push(RapidHttpBacked):
     def handle_request(self, request):
         if request.method != 'POST':
             return HttpResponse('Not a post!')
-        #self.debug('This is the entire request (raw): %s' % request)
         self.debug('This is the PUSH inbound POST data: %s' % request.raw_post_data)
         message = self.message(request.POST)
         if message:
@@ -40,15 +41,91 @@ class Push(RapidHttpBacked):
         now = datetime.datetime.utcnow()
         return super(Push, self).message(mobile_number, text, now)
         self.debug(data)
-    def send(self, message):
-#    base_url = 'http://api.tropo.com/1.0/sessions'
-#    token = '1c58694261c3714c9598abaa13d22bdc9c859f31383805bb8445a3fe40e0e19d3017b06ed4e56ae582695210'        # Insert your token here
-#    token = '8dedf13d8330b24dade023972bf72db1eb347eefbcbf46aea81ea94a794385eeba9951d94debd9918b5f61b3'
-#    action = 'create'
-#    number = '14122677933'    
-#
-#    params = urlencode([('action', action), ('token', token), ('numberToDial', message.connection.identity), ('msg', message.text)])
-#    self.debug("%s?%s" % (base_url, params))
-#    data = urlopen('%s?%s' % (base_url, params)).read()
-     self.debug(message)
 
+    def send(self, message):
+        """
+        <methodCall>
+            <methodName>EAPIGateway.SendSMSItem</methodName>
+            <params>
+                <param>
+                    <value>
+                        <struct>
+                            <member>
+                                <name>Password</name>
+                                <value>p487fhem</value>
+                            </member>
+                            <member>
+                                <name>Channel</name>
+                                <value><int>9840</int></value>
+                            </member>
+                            <member>
+                                <name>Service</name>
+                                <value><int>124524</int>
+                                </value>
+                            </member>
+                            <member>
+                                <name>SMSText</name>
+                                <value>[text]</value>
+                            </member>
+                            <member>
+                                <name>Numbers</name>
+                                <value>[number]</value>
+                            </member>                        
+                        </struct>
+                    </value>
+                </param>
+            </params>
+        </methodCall>    
+        """
+        url = "https://dragon.operatelecom.com:1089/Gateway"
+        password = "p487fhem"
+        channel = "9840"
+        service = "124524"
+        numbers = message.connection.identity
+        text = message.text
+        
+        root = ET.Element("methodCall")
+        
+        methodName = ET.SubElement(root, "methodName")
+        methodName.text = "EAPIGateway.SendSMS"
+        params = ET.SubElement(root, "params")
+        param = ET.SubElement(params, "param")
+        paramValue = ET.SubElement(param, "value")
+        struct = ET.SubElement(paramValue, "struct")
+        member1 = ET.SubElement(struct, "member")
+        member1Name = ET.SubElement(member1, "name")
+        member1Name.text = "Password"    
+        member1Value = ET.SubElement(member1, "value")
+        member1Value.text = password
+        member2 = ET.SubElement(struct, "member")
+        member2Name = ET.SubElement(member2, "name")
+        member2Name.text = "Channel"    
+        member2Value = ET.SubElement(member2, "value")
+        member2Int = ET.SubElement(member2Value, "int")
+        member2Int.text = channel
+        member3 = ET.SubElement(struct, "member")
+        member3Name = ET.SubElement(member3, "name")
+        member3Name.text = "Service"    
+        member3Value = ET.SubElement(member3, "value")
+        member3Int = ET.SubElement(member3Value, "int")
+        member3Int.text = service
+        member4 = ET.SubElement(struct, "member")
+        member4Name = ET.SubElement(member4, "name")
+        member4Name.text = "Numbers"    
+        member4Value = ET.SubElement(member4, "value")
+        member4Value.text = numbers
+        member5 = ET.SubElement(struct, "member")
+        member5Name = ET.SubElement(member5, "name")
+        member5Name.text = "SMSText"    
+        member5Value = ET.SubElement(member5, "value")
+        member5Value.text = text
+        
+        req = urllib2.Request(url=url, 
+                              data=ET.tostring(root), 
+                              headers={'Content-Type': 'application/xml'})
+        try:
+            handle = urllib2.urlopen(req)
+        except e:
+            print e   
+        print  HttpResponse(handle.read())
+        return HttpResponse(handle.read())
